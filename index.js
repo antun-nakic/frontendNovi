b92b8168ec62a15c825e4f29d25890f7d7215c76
DanielusMagnificus <like3venom@gmail.com>
2022-04-26 18:49:27 +0200

    moved the movie update function to index.js; added script tag in index.html

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("bok");
+
+// function which updates the movie list; takes 1 query param for movie name
+function novaLista(upit) {
+  var ubaciOde = document.getElementById("podaci");
+  fetch(`https://api.tvmaze.com/search/shows?q=${upit}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var html = "";
+      data.forEach(function (movie) {
+        html += "<p>" + movie.show.name + "</p>";
+      });
+      ubaciOde.innerHTML = html;
+    });
+}

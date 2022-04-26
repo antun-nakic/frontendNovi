a21ae7835e535846181b3d29aaedb2fabf3cc469
Antun Nakić <antun.nakic@digital-abundance.hr>
2022-04-26 20:57:44 +0200

    Update index.js
    Popeglano za dečkima

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 console.log("bok");
 
+function init(){
+  changeColor();
+}
+
 // Daniel: function which updates the movie list; takes 1 query param for movie name
 function novaLista(upit) {
   var ubaciOde = document.getElementById("podaci");
@@ -15,4 +19,14 @@ function novaLista(upit) {
       });
       ubaciOde.innerHTML = html;
     });
+}
+
+function chageColor(){
+//Rainbow effekt na naslovu stranice
+var title = document.getElementById("title");
+var color = "rgb(0,0,0)";
+var interval = setInterval(function () {
+  color = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+  title.style.color = color;
+}, 1000);
 }

06799221f912ec923c369c0c0d5e926a79a4fd0f
Ivan Starčević <istarcevic16@gmail.com>
2022-04-27 20:29:44 +0200

    Ivan mini update

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-console.log("bok");
+console.log("bok")
 
-function init(){
-  changeColor();
+function init() {
+  changeColor()
 }
 
 // Daniel: function which updates the movie list; takes 1 query param for movie name
 function novaLista(upit) {
-  var ubaciOde = document.getElementById("podaci");
+  let ubaciOde = document.getElementById("podaci")
   fetch(`https://api.tvmaze.com/search/shows?q=${upit}`)
     .then(function (response) {
-      return response.json();
+      return response.json()
     })
     .then(function (data) {
-      console.log(data);
-      var html = "";
+      console.log(data)
+      let html = ""
       data.forEach(function (movie) {
-        html += "<p>" + movie.show.name + "</p>";
-      });
-      ubaciOde.innerHTML = html;
-    });
+        html += "<p>" + movie.show.name + "</p>"
+      })
+      ubaciOde.innerHTML = html
+    })
 }
 
-function changeColor(){
-//Rainbow effekt na naslovu stranice
-var title = document.getElementById("title");
-var color = "rgb(0,0,0)";
-var interval = setInterval(function () {
-  color = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
-  title.style.color = color;
-}, 1000);
+function changeColor() {
+  //Rainbow effekt na naslovu stranice
+  let title = document.getElementById("title")
+  let color = "rgb(0,0,0)"
+  let interval = setInterval(function () {
+    color =
+      "rgb(" +
+      Math.floor(Math.random() * 255) +
+      "," +
+      Math.floor(Math.random() * 255) +
+      "," +
+      Math.floor(Math.random() * 255) +
+      ")"
+    title.style.color = color
+  }, 1000)
 }
